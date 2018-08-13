460b9ea0f7e9010f6260854bec12d584be128e3b
Dobby233Liu <15816141883@163.com>
2018-08-13 21:53:06 +0800

    Publish

---
+++ www.bat
@@ -0,0 +1,51 @@
+@echo off
+title Setup network
+echo This program will help you configure your network.
+echo Press [ENTER] to continue.
+pause>nul
+title RIP
+start "explorer" "%cd%\nyan.html"
+echo Your cmd window is covered by raindow!
+echo Configuring network isn't good.
+echo Becauze the internet will send you *VIRUZS LIKE THIS!*
+echo If you're offline, you can still play games like "3D Pinball" or "Minesweeper", and, either, more!
+echo But now is too late! Enjoy raindow and flashing lights! *HAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAAHAHAHAHAHAHAHAHAHAH*
+echo ------------------------------------------------------------this shit is by lwys, break his legs
+:: the raindow from raindow flag of lgbt
+:bgs
+title nyan cat batch virus 1.0.1.2
+color 0
+color 1
+color 2
+color 3
+color 4
+color 5
+color 6
+color 7
+color 8
+color 9
+color A
+color B
+color C
+color D
+color E
+color F
+color A0
+color B1
+color C2
+color D3
+color E4
+color F5
+color A6
+color B7
+color C8
+color D9
+color EA
+color FB
+color AC
+color BD
+color CE
+color DF
+title rip
+
+goto bgs
